06b5b5400a74fb20cc29c5bf2cb1854c0cedc7a4
Hidayat arghandabi <hidayatarg@gmail.com>
2018-09-20 10:51:19 +0300

    Db Context and ComicBook Model created

---
+++ ComicBooks/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComicBooks.Models;
+
+namespace ComicBooks.Data
+{
+   public class Context:DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+# NuGET install EntityFramework
+
